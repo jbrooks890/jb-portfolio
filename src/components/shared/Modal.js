cfd1ec8074893ad488a0428f0c0652871dc05586
ArcanBoi <61268478+ArcanBoi@users.noreply.github.com>
2022-10-01 00:29:37 -0400

    added resume to modal

--- src/components/shared/Modal.js
+++ src/components/shared/Modal.js
@@ -15,7 +15,7 @@ export default function Modal({ isShowing, hide, children }) {
             tabIndex={-1}
             role="dialog"
             // onClick={hide}
-            onKeyDown={e => (e.key === "Escape" ? hide() : null)}
+            // onKeyDown={e => (e.key === "Escape" ? hide() : null)}
           >
             <div className="modal">
               <div
