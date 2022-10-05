a4238eecc5cf09da35724cfe03cc044ea691e244
Julian Brooks <julian@Julians-MacBook-Air.local>
2022-10-05 00:26:21 -0400

    minors

--- src/components/sections/Contact.js
+++ src/components/sections/Contact.js
@@ -1,12 +1,24 @@
 import "../../styles/Contact.css";
+import ConnectForm from "../frags/ConnectForm";
+import useModal from "../hooks/useModal";
+import Modal from "../shared/Modal";
 
 export default function Contact() {
+  const { isShowing, toggle } = useModal();
+
   return (
-    <section id="contact" className="site-section flex col">
-      <div id="contact-content-wrapper" className="flex col">
-        <h2>Let's Connect</h2>
-        <button className="pill">Email me!</button>
-      </div>
-    </section>
+    <>
+      <section id="contact" className="site-section flex col">
+        <div id="contact-content-wrapper" className="flex col">
+          <h2>Let's Connect</h2>
+          <button className="pill" onClick={toggle}>
+            Email me!
+          </button>
+        </div>
+      </section>
+      <Modal isShowing={isShowing} hide={toggle}>
+        <ConnectForm />
+      </Modal>
+    </>
   );
 }
