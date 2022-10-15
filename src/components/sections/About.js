21579a51efdae867a816ceab10a591a297e79d53
ArcanBoi <61268478+ArcanBoi@users.noreply.github.com>
2022-10-15 15:26:30 -0400

    minors

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -17,28 +17,7 @@ export default function About({ pages }) {
   return (
     <section id="about" className="site-section flex col">
       <div className="content-wrap grid">
-        {/* <div className="description flex col">
-          <h2>A Developer</h2>
-          <p>
-            I'm Julian! I am a <em>software developer</em> based out of{" "}
-            <i>Atlanta, GA</i>. I enjoy the duality of technical and artistic
-            design and how they can be used together to solve problems.
-          </p>
-          <ul className="about-shift">
-            <h3>I'm also</h3>
-            {pages.map((page, i) => (
-              <li className={page === siteMode ? "active" : ""} key={i}></li>
-            ))}
-          </ul>
-        </div> */}
         <Blurb mode={siteMode} pages={pages} content={blurbs} />
-        <div className="headshot-wrap flex">
-          <img
-            src={require("../../assets/images/julian_profile_pic.jpg")}
-            alt="Headshot of Julian"
-            className="headshot"
-          />
-        </div>
         <div className="about-body flex col">
           <SkillCache concise={false} showIcons={true} featured={true} />
           <button className="resume-button" onClick={() => toggle()}>
